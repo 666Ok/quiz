c673652a9a2484c47ee63417f9b81e360262bd1c
666Ok <mmajtanm@gmail.com>
2022-02-16 20:29:14 +0100

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import "./Quiz.css";
+
+export default function Quiz({
+  data,
+  setStop,
+  questionNumber,
+  setQuestionNumber,
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(500, () => {
+      setClassName(a.correct ? "answer correct" : "answer wrong");
+    });
+    delay(500, () => {
+      setClassName(a.correct ? "answer correct" : "answer wrong");
+    });
+    delay(2000, () => {
+        if(a.correct){
+            setQuestionNumber(prev=>prev + 1);
+            setSelectedAnswer(null);
+        } else{
+            setStop(true);
+        }
+      });
+  };
+
+  return (
+    <div className="quiz">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => handleClick(a)}
+          >
+            {a.text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
